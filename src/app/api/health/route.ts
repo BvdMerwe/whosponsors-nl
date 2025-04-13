9796c7a66828f73c0ea7dd5372c50c1ca1d13c66
Bernardus <bernard@getbaraka.com>
2025-04-13 21:42:18 +0200

    Fix route.

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export default async function GET(): Promise<NextResponse> {
+export async function GET(): Promise<NextResponse> {
     return NextResponse.json({
         status: "healthy",
     });
